7ec1ab9e8ef27aaec8ec1e03e545e35dfa3f4fae
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-10-13 11:25:42 +0300

    add task_2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+start:                                          //метка
+Console.Clear();
+
+Console.Write("Введите число или 0 чтобы выйти из программы: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if(number == 0){
+    return;
+}
+
+int result = 0;
+while(number > 0){
+    int dig = number % 10;
+    result +=  dig;
+    number /= 10;
+}
+    
+Console.WriteLine($"Сумма цифр равна: {result}.");
+
+Thread.Sleep(3000);                             //задержка вывода и повторный запуск программы
+goto start;
