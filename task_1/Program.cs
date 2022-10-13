4a0638ab74943b0aa600ef6a1a20c1a98a0faa6b
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-10-13 11:00:29 +0300

    add task_1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int number1 = GetNumber();
+int number2 = GetNumber();
+int res = 1;
+
+if (number2 < 0){
+    Console.WriteLine("Введено не натуральное число.");
+}
+else
+{
+    Console.WriteLine($"Результат возведения числа {number1} в степень {number2} равен: {ExpNumber(number1, number2)}");
+}
+
+int GetNumber(){
+    Console.Write("Введите число: ");
+    int number = int.Parse(Console.ReadLine());
+ return number;
+}
+int ExpNumber(int num1, int num2){
+    for(int i = 0; i < num2; i++){
+        res *= num1;
+    }
+    return res;
+}
